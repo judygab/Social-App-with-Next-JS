7ecb03409327e0e6b4eadd00461c4e05c609bd33
Ivditi Gabeskiria <ivditigabeskiria@ivditis-air.home>
2020-08-12 22:52:01 -0400

    added ts and drawer

---
+++ social-app/pages/index.tsx
@@ -0,0 +1,50 @@
+// pages/index.tsx
+import React from "react";
+import { GetServerSideProps } from "next";
+import {
+  AuthTokens,
+  useAuth,
+  useAuthFunctions,
+  getServerSideAuth,
+} from "../auth";
+import Button from '@material-ui/core/Button';
+import Link from 'next/link';
+import PersistentDrawerLeft from '../components/PersistentDrawerLeft';
+
+const Home = (props: { initialAuth: AuthTokens }) => {
+  const auth = useAuth(props.initialAuth);
+  //useAuth hook is supposed to only be called once per page. Not recommended to add the Auth information to `_app.tsx`, as that would force all sites to use server-side rendering!
+  //TODO: create context for auth;
+  const { login, logout } = useAuthFunctions();
+
+  return (
+    <React.Fragment>
+      <div>
+      <PersistentDrawerLeft />
+      <Button variant="contained" color="primary">
+        Primary
+      </Button></div>
+      {auth ? (
+        <button type="button" onClick={() => logout()}>
+          sign out
+        </button>
+      ) : (
+        <React.Fragment>
+          <button type="button" onClick={() => login()}>
+            sign in
+          </button>
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps<{
+  initialAuth: AuthTokens;
+}> = async (context) => {
+  const initialAuth = getServerSideAuth(context.req); //returns the auth info of the current user
+
+  return { props: { initialAuth } };
+};
+
+export default Home;
